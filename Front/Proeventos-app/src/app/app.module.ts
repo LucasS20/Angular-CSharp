d0f789006a8d249ed337262622616339c7d98652
lucas.santos <lucassr614@gmail.com>
2023-10-06 14:40:03 -0300

    add bootstrap and component styles

--- Front/Proeventos-app/src/app/app.module.ts
+++ Front/Proeventos-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {EventosComponent} from './eventos/eventos.component';
+import {PalestrantesComponent} from './palestrantes/palestrantes.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
