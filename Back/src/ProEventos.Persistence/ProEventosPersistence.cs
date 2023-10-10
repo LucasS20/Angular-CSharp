681afff43e81a236146ca3aefd121f78d19b6cf0
lucas.santos <lucassr614@gmail.com>
2023-10-10 17:21:12 -0300

    starting persistence class

---
+++ Back/src/ProEventos.Persistence/ProEventosPersistence.cs
@@ -0,0 +1,68 @@
+﻿using ProEventos.Domain;
+
+namespace ProEventos.Persistence;
+
+public class ProEventosPersistence : IProEventosPersistence
+{
+    private readonly ProEventosContext _context;
+
+    public ProEventosPersistence(ProEventosContext context)
+    {
+        this._context = context;
+    }
+
+    public void Add<T>(T entity) where T : class
+    {
+        _context.Add(entity);
+    }
+
+    public void Delete<T>(T entity) where T : class
+    {
+        _context.Remove(entity);
+    }
+
+    public void DeleteRange<T>(object[] entityArray) where T : class
+    {
+        _context.RemoveRange(entityArray);
+    }
+
+    public void Update<T>(T entity) where T : class
+    {
+        _context.Update(entity);
+    }
+
+    public async Task<bool> SaveChangesAsync()
+    {
+        return (await _context.SaveChangesAsync()) > 0;
+    }
+
+    public Task<Event[]> GetAllEventsByThemeAsync(string tema, bool includeSpeaker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Event[]> GetAllEventsAsync(bool includeSpeaker = false)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Event[]> GetEventByIdAsync(int eventoId, bool includeSpeaker)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Event[]> GetAllSpeakersByNameAsync(string name, bool includeEvents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Event[]> GetAllSpeakersAsync(bool includeEvents)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Event[]> GetSpeakerByIdAsync(int eventoId, bool includeEvents)
+    {
+        throw new NotImplementedException();
+    }
+}
