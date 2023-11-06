8751a16d6184cce9c6ef4097747905945756ffa1
lucas.santos <lucassr614@gmail.com>
2023-11-06 14:29:33 -0300

    updates backend

---
+++ Back/src/ProEventos.Repository/Interfaces/ISocialMediaPersist.cs
@@ -0,0 +1,14 @@
+﻿using ProEventos.Domain;
+
+namespace ProEventos.Persistence.Interfaces;
+
+public interface ISocialMediaPersist
+{
+    Task<SocialMedia> GetSocialMediaEventoIdAsync(int eventoId, int socialMediaId);
+
+    Task<SocialMedia> GetSocialMediaSpeakerIdAsync(int speakerId, int socialMediaId);
+
+    Task<SocialMedia[]> GetAllSocialMediaEventoAsync(int eventoId);
+
+    Task<SocialMedia[]> GetAllSocialMediaSpeakerAsync(int speakerId);
+}
